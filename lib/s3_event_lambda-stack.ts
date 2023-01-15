771cbabafd36086dad5d0900d548bd135fc9ea97
hiroya.shimamoto <shshimamo@gmail.com>
2023-01-15 21:21:08 +0900

    update stack

--- lib/s3_event_lambda-stack.ts
+++ lib/s3_event_lambda-stack.ts
@@ -1,4 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
+import * as eventsources from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as go from '@aws-cdk/aws-lambda-go-alpha'
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +9,22 @@ export class S3EventLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3
+    const bucket = new s3.Bucket(this, 'Bucket', {
+      bucketName: 's3-event-lambda-bucket',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3EventLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda
+    const lambda = new go.GoFunction(this, 'RdsProxyHandler', {
+      entry: 'lambda',
+      timeout: cdk.Duration.seconds(300)
+    });
+
+    lambda.addEventSource(new eventsources.S3EventSource(bucket, {
+      events: [ s3.EventType.OBJECT_CREATED ],
+      // filters: [ { prefix: 'subdir/' } ]
+    }));
   }
 }
